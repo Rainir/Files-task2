de819d07cdca6aec4af03afbf48a0770737cd557
Rainir <off_rainir@proton.me>
2022-12-21 22:53:24 +0300

    ZipFiles added

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,14 @@ import java.util.zip.ZipOutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> pathlist = new ArrayList<>();
+        List<String> pathList = new ArrayList<>();
         GameProgress gamer1 = new GameProgress(100, 3, 20, 3.8);
         GameProgress gamer2 = new GameProgress(60, 4, 12, 15.3);
         GameProgress gamer3 = new GameProgress(120, 3, 24, 5.1);
-        saveGame(gamer1, "F://Game/savegame/save1.dat", pathlist);
-        saveGame(gamer2, "F://Game/savegame/save2.dat", pathlist);
-        saveGame(gamer3, "F://Game/savegame/save3.dat", pathlist);
-        System.out.println(pathlist);
-        zipFiles("F://Game/savegame/zip.zip", pathlist);
+        saveGame(gamer1, "F://Game/savegame/save1.dat", pathList);
+        saveGame(gamer2, "F://Game/savegame/save2.dat", pathList);
+        saveGame(gamer3, "F://Game/savegame/save3.dat", pathList);
+        zipFiles("F://Game/savegame/zip.zip", pathList);
     }
 
     public static void saveGame(GameProgress gamer, String path, List<String> pathList) {
@@ -27,17 +26,25 @@ public class Main {
         }
     }
 
-    public static void zipFiles(String pathZip, List<String> path) {
+    public static void zipFiles(String pathZip, List<String> pathList) {
+        File file;
         try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(pathZip))) {
-            for (int i = 0; i < path.size(); i++) {
-                System.out.println(path.get(i));
-                FileInputStream fis = new FileInputStream(path.get(i));
-                ZipEntry entry  = new ZipEntry("save" + (i + 1) + ".dat");
+            for(String path : pathList) {
+                FileInputStream fis = new FileInputStream(path);
+                String[] parts = path.split("/");
+                String fileName = parts[parts.length - 1];
+                ZipEntry entry = new ZipEntry(fileName);
                 zout.putNextEntry(entry);
                 byte[] buffer = new byte[fis.available()];
                 fis.read(buffer);
                 zout.write(buffer);
                 zout.closeEntry();
+                file = new File(path);
+                file.delete();
+                if (file.delete())
+                    System.out.println(fileName + " добавлен в архив и удален");
+                else
+                    System.out.println("Ошибка, не могу удалить " + fileName);
             }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
